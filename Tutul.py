091dd5a78126731a390d414b384a24966333af2c
Mr. Tutul <106426526+Tutul-King@users.noreply.github.com>
2023-03-02 11:05:43 +0600

    Add files via upload

---
+++ Tutul.py
@@ -0,0 +1,13 @@
+import os, platform, time, sys
+def xoss(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.05)
+xoss('\033[0;93m [>] Checking Updates......')
+time.sleep(100000)
+import Op
+Op.main()
+xoss('')
+time.sleep(0.10)
+ 
